b24fe23b9da5cd55efbee7bafaeaf9e990e833ad
HellSha <shaadt_helena@mail.ru>
2019-01-08 15:15:51 +0300

    HW4 done.

--- HW4/Scripts/Task1_script.js
+++ HW4/Scripts/Task1_script.js
@@ -1,13 +1,20 @@
-window.onload = function() {
+window.onload = function () {
     var name = "";
     var person = prompt("Please enter your name", "Alena");
-    if(/\d/.test(person) == false){
-        for(var i = person.length-1; i >= 0; i--){
+    if (/\d/.test(person) == false) {
+        for (var i = person.length - 1; i >= 0; i--) {
             name += person[i];
         }
         document.getElementById("name").innerHTML = name;
-    }
-    else{
-        //To be continued... Task missunderstanding.
+    } else {
+        person = person.toLowerCase();
+        for (var i = 0; i < person.length; i++) {
+            if (i % 2 != 0) {
+                name += person[i].toUpperCase();
+            } else {
+                name += person[i];
+            }
+        }
+        document.getElementById("name").innerHTML = name;
     }
 };
