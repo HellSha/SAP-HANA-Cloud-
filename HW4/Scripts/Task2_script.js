0e7c2e3ac35d599c2496e144a90c59f98ee99f19
HellSha <shaadt_helena@mail.ru>
2019-01-08 13:15:35 +0300

    HW4 was added. TODO: HW2 task1_2; HW3 Bootstrap.

---
+++ HW4/Scripts/Task2_script.js
@@ -0,0 +1,10 @@
+function openNewPage() {
+    window.open("https://getbootstrap.com/docs/4.0/components/buttons/");
+}
+
+function drawFlexbox() {
+    document.body.innerHTML = "";
+    var link = document.querySelector('link[rel=import]');
+    var content = link.import.querySelector('.Rtable');
+    document.body.appendChild(content.cloneNode(true));
+}
