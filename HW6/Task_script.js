6e585514d27b12ae9d1aa8f73b6897745f37cba0
HellSha <shaadt_helena@mail.ru>
2019-01-21 12:54:18 +0300

    HW6 was added.

---
+++ HW6/Task_script.js
@@ -0,0 +1,85 @@
+function showData(xhr) {
+    var array = JSON.parse(xhr.responseText);
+    var myHTMLStr = '<table>';
+    myHTMLStr += '<thead><tr><th>UID</th><th>ID</th><th>Name</th><th>Email</th><th>Body</th><th colspan="2">Edit/Delete</th></tr></thead>';
+    if (Array.isArray(array)) {
+        for (var i in array) {
+            myHTMLStr += '<tr><td>' + array[i]['postId'] + '</td><td>' +
+                array[i]['id'] + '</td><td>' +
+                array[i]['name'] + '</td><td>' +
+                array[i]['email'] + '</td><td>' +
+                array[i]['body'] + '</td> <td>' +
+                '<button class="btn btn-danger" id="deleteBtn" onclick="deleteRequest()">Delete</button></td><td>' +
+                '<button class="btn btn-warning" id="putBtn" onclick="drawForm()">Edit</button></td></tr>';
+        }
+    } else {
+        myHTMLStr += '<tr><td>' + array.postId + '</td><td>' +
+            array.id + '</td><td>' +
+            array.name + '</td><td>' +
+            array.email + '</td><td>' +
+            array.body + '</td> <td class="btn btn-danger">' +
+            '<button id="deleteBtn" onclick="deleteRequest()">Delete</button></td><button class="btn btn-warning"' +
+            '<button id="putBtn" onclick="drawForm()">Edit</button></td></tr>';
+    }
+    myHTMLStr += '</table>';
+    document.getElementById('table').innerHTML = myHTMLStr;
+}
+
+function getRequest() {
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            showData(this);
+            console.log(this.responseText);
+        }
+    };
+    request.open('GET', 'https://jsonplaceholder.typicode.com/comments');
+    request.send();
+}
+
+function postRequest() {
+    var params = {};
+    params.postId = document.getElementById('userId').value;
+    params.name = document.getElementById('name').value;
+    post.email = document.getElementById('email').value;
+    post.body = document.getElementById('comment').value;
+    // return post;
+
+    var request = new XMLHttpRequest();
+    var jsonData = JSON.stringify(params);
+
+    request.open('POST', 'https://jsonplaceholder.typicode.com/comments', true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(params);
+
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert('Changes saved!');
+            console.log(this.responseText);
+        }
+    };
+}
+
+function deleteRequest(){
+    var request = new XMLHttpRequest();
+    
+    var id = comment.id;
+     request.open('DELETE', 'https://jsonplaceholder.typicode.com/comments/' + id);
+   request.send();
+    
+     request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert('Comment was successfully deleted!');
+            console.log(this.responseText);
+        }
+    };   
+}
+
+function drawForm() {
+    document.body.innerHTML = "";
+    var link = document.querySelector('link[rel=import]');
+    var content = link.import.querySelector('#postForm');
+    document.body.appendChild(content.cloneNode(true));
+    //document.getElementById('commentId').value = comment.id;
+}
